0be8732c7646bb34cf8cb69b679c6ab279c76640
philip72 <philipwamai85@gmail.com>
2023-07-20 12:16:14 +0300

    madlib

---
+++ philip_fd/madlibs.py
@@ -0,0 +1,11 @@
+#blanks in a praggraph 
+#string concat
+
+verb1=input("write yout first verb: ")
+verb2=input("write your second verb ")
+noun1=input("write your first noun: ")
+noun2=input("write your second noun ")
+madlib= f"Do not {verb1} the power {noun1} with hands when using the{noun2}\
+mode power supply or when just {verb2} to the {noun1} mode power supply"
+
+print(madlib)
