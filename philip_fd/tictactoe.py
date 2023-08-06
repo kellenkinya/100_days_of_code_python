ff94a723a1e1630a6443e4115a2d71eb1ca66ff3
philip72 <philipwamai85@gmail.com>
2023-08-06 11:33:45 +0300

    psudeocode for tictac toe

---
+++ philip_fd/tictactoe.py
@@ -0,0 +1,13 @@
+#pseudocode 
+"""
+two players: computer and human player
+
+computer will have x and human will o 
+
+we need a board 3*3 board drawn separated by lines
+
+there is checking whose move we at , 
+check if a square is a avaialble 
+how to win 
+how to continue
+"""
